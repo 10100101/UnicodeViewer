c55f61a96a98fbbfb3d4b6dad20147bccb5dc54b
Ulrich von Poblotzki <ulrich@poblotzki.de>
2011-07-03 14:56:49 +0200

    Implemented Simple Version

---
+++ UnicodeViewer/UnicodeListViewController.h
@@ -0,0 +1,16 @@
+//
+//  UnicodeListViewController.h
+//  UnicodeViewer
+//
+//  Created by Ulrich von Poblotzki on 03.07.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface UnicodeListViewController : UITableViewController {
+    
+}
+
+@end
