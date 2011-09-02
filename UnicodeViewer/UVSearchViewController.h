37657b6eced898071884ca8e32da4bb2e0766a68
Ulrich von Poblotzki <ulrich@poblotzki.de>
2011-09-02 21:25:00 +0200

    Added search view (not implemented)

---
+++ UnicodeViewer/UVSearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  UVSearchViewController.h
+//  UnicodeViewer
+//
+//  Created by Ulrich von Poblotzki on 02.09.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UVSearchViewController : UITableViewController <UISearchBarDelegate, UISearchDisplayDelegate>
+
+@end
