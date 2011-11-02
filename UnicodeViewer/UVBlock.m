b42980f34292a08bf65344f135999d4c5835c693
Ulrich von Poblotzki <ulrich@poblotzki.de>
2011-11-02 21:23:23 +0100

    Changed (c) headers

--- UnicodeViewer/UVBlock.m
+++ UnicodeViewer/UVBlock.m
@@ -3,7 +3,20 @@
 //  UnicodeViewer
 //
 //  Created by Ulrich von Poblotzki on 21.10.11.
-//  Copyright (c) 2011 10100101.com. All rights reserved.
+//  Copyright 2011 Ulrich von Poblotzki.
+//
+//  UnicodeViewer is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  UnicodeViewer is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with UnicodeViewer.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 #import "UVBlock.h"
--- UnicodeViewer/UVChar+Favorits.h
+++ UnicodeViewer/UVChar+Favorits.h
@@ -1,9 +1,23 @@
 //
-//  Created by uli on 28.10.11.
+//  UVChar+Favorits.h
+//  UnicodeViewer
 //
-// To change the template use AppCode | Preferences | File Templates.
+//  Created by Ulrich von Poblotzki on 21.10.11.
+//  Copyright 2011 Ulrich von Poblotzki.
 //
-
+//  UnicodeViewer is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  UnicodeViewer is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with UnicodeViewer.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 #import <Foundation/Foundation.h>
 #import "UVChar.h"
